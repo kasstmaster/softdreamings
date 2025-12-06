3c8f63dbad88338872ab0cbc8fec97ab6be6f071
kasstmaster <kassandraraudman@gmail.com>
2025-12-05 17:42:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -649,8 +649,13 @@ class GameNotificationSelect(discord.ui.Select):
 class GameNotificationView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
-    @discord.ui.button(label="Get Notified", style=discord.ButtonStyle.grey, custom_id="persistent_game_notif_button")
-    async def button(self, button: discord.ui.Button, interaction: discord.Interaction):
+
+    @discord.ui.button(
+        label="Get Notified",
+        style=discord.ButtonStyle.grey,
+        custom_id="game_notif_persistent_button_123"
+    )
+    async def open_menu(self, button: discord.ui.Button, interaction: discord.Interaction):
         await interaction.response.send_message(
             "Choose the game notifications you want:",
             view=GameNotificationSelectView(),
